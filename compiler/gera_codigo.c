b79c1f2aa90a94e979c2ab3ffc8c813c5fb70736
Eduardo Dantas Luna <michaelthefear@pop-os.localdomain>
2022-05-30 12:50:05 -0300

    generate header and footer created

--- compiler/gera_codigo.c
+++ compiler/gera_codigo.c
@@ -5,3 +5,6 @@ typedef int (*funcp) (int x);
 void gera_codigo (FILE *f, unsigned char code[], funcp *entry);
 unsigned char * lbsToMachineCode(char *lbs);
 void runMachineCode(unsigned char*lbs,int size);
+unsigned char * generateFunctionHeader(int size);
+unsigned char * generateFunctionFooter();
+
