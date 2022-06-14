dcabdbeb0c2d59b427d7eaccdf241b2ba5ffd0e0
Eduardo Dantas <edudaluna2000@gmail.com>
2022-06-13 21:14:12 -0300

    Tables and notes about converting assembly to machine code

--- lbsASM/main.s
+++ lbsASM/main.s
@@ -2,8 +2,15 @@ function1:
 
 pushq %rbp
 movq %rsp, %rbp
-movl $1, %r9d
-movl %r9d, %r9d
-movl %r9d, %eax
+
+
+imul $0xFFFF, %rax
+imul $0xFFFF, %rcx
+imul $0xFFFF, %rdx
+imul $0xFFFF, %rsi
+imul $0xFFFF, %rdi
+imul $0xFFFF, %r8
+imul $0xFFFF, %r9
+
 popq %rbp
 ret	
