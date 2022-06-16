862d424467cb756b1192116ab127774bdaaafbf6
Eduardo Dantas <edudaluna2000@gmail.com>
2022-06-15 22:26:02 -0300

    Table of LBS to MACHINE code (just finished operations)

--- lbsASM/main.s
+++ lbsASM/main.s
@@ -1,4 +1,21 @@
-mov 0xFFF,%rax
-pop %rbp
-ret
+/*rax rcx rdx rsi rdi r8 r9*/
+function1:
+    pushq %rbp
+    mov %rsp, %rbp
+    mov $0xfff,%r12
+    mov %r12,%rdi
+    call function2
+    mov %rdi,%rax
+    popq %rbp
+    ret
+
+function2:
+    pushq %rbp
+    mov %rsp, %rbp
+    mov $0x0,%r12
+    mov %r12,%rdi
+    mov $0x2,%rax
+    popq %rbp
+    ret
+
 
