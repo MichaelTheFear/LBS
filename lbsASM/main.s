39917b638f5e5e2f3d4a2eff3022cb47ddf4913a
Eduardo Dantas <edudaluna2000@gmail.com>
2022-06-16 17:29:17 -0300

    Starting Converting register to stack

--- lbsASM/main.s
+++ lbsASM/main.s
@@ -1,21 +1,27 @@
-/*rax rcx rdx rsi rdi r8 r9*/
+/*rax r12 rdx rsi rdi r8 r9*/
 function1:
-    pushq %rbp
-    mov %rsp, %rbp
-    mov $0xfff,%r12
-    mov %r12,%rdi
-    call function2
-    mov %rdi,%rax
-    popq %rbp
-    ret
+pushq %rbp
+movq %rsp,%rbp
+subq $16,%rsp
+movq %rbx,-8(%rbp)
+movq %r12,-16(%rbp)
+movq $12,%r12
+call function2
+movq %r12,%rax
+movq -8(%rbp),%rbx
+movq -16(%rbp),%r12
+leave
+
 
 function2:
-    pushq %rbp
-    mov %rsp, %rbp
-    mov $0x0,%r12
-    mov %r12,%rdi
-    mov $0x2,%rax
-    popq %rbp
-    ret
+pushq %rbp
+movq %rsp,%rbp
+subq $16,%rsp
+movq $15,%r12
+movq %r12,%rax
+movq -8(%rbp),%rbx
+movq -16(%rbp),%r12
+leave
+
 
 
