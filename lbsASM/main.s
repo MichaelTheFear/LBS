1c66c96a5db5aef3ce03f7299bb9dfcf0b3ae46d
Eduardo Dantas <edudaluna2000@gmail.com>
2022-06-16 17:58:02 -0300

    cm command creted (run machine code from a assembly file in c)

--- lbsASM/main.s
+++ lbsASM/main.s
@@ -1,27 +1,10 @@
 /*rax r12 rdx rsi rdi r8 r9*/
 function1:
 pushq %rbp
-movq %rsp,%rbp
-subq $16,%rsp
-movq %rbx,-8(%rbp)
-movq %r12,-16(%rbp)
-movq $12,%r12
-call function2
-movq %r12,%rax
-movq -8(%rbp),%rbx
-movq -16(%rbp),%r12
-leave
-
-
-function2:
-pushq %rbp
-movq %rsp,%rbp
-subq $16,%rsp
-movq $15,%r12
-movq %r12,%rax
-movq -8(%rbp),%rbx
-movq -16(%rbp),%r12
-leave
+mov %rsp,%rbp
+mov $12,%rax
+popq %rbp
+ret
 
 
 
