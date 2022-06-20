b61b2a36d20dc83da6323e7b46a2525a68280ae6
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2022-06-19 21:42:02 -0300

    removeFirstChar function fixed according to new definition of string

--- playgrounds/playgroundMadeira.c
+++ playgrounds/playgroundMadeira.c
@@ -3,7 +3,11 @@
 #include <string.h>
 
 typedef int (*funcp)(int x);
-typedef char *string;
+typedef struct __string
+{
+    int len;     // tamanho da string
+    char *value; // a string em si
+} string;
 typedef unsigned char byte;
 
 // funcoes auxiliares
@@ -14,67 +18,74 @@ string removeFirstChar(string s);
 
 int main(void)
 {
-    // using littleThatEndian function
-    // byte *bytes = littleThatEndian((byte *)&x, 1);
-    // printf("%d\n", bytes[0]);
-    // printf("%d\n", bytes[1]);
-    // printf("%d\n", bytes[2]);
-    // printf("%d\n", bytes[3]);
+    // byte *bytes;
+    // int num = 5;
 
-    string a = removeFirstChar("teste");
-    printf("%s\n", a);
+    // bytes = intToBytes(num, 1);
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     printf("%d ", bytes[i]);
+    // }
+    // printf("\n");
+
+    string buffer = {5, "teste"};
+    string newstr;
+
+    newstr = removeFirstChar(buffer);
+    printf("%s\n", newstr.value);
 
     return 0;
 }
 
 string removeFirstChar(string s)
 {
-    string new_str;
-    new_str = (string)malloc(sizeof(char) * (strlen(s) - 1));
-    int i;
+    string newstr;
+    newstr.len = s.len - 1;
+    newstr.value = (char *)malloc(sizeof(char) * newstr.len);
 
-    for (i = 0; i < strlen(s); i++)
+    for (int i = 0; i < s.len; i++)
     {
-        new_str[i] = s[i + 1];
+        newstr.value[i] = s.value[i + 1];
     }
 
-    new_str[i] = '\0';
+    newstr.value[newstr.len] = '\0';
 
-    return new_str;
+    return newstr;
 }
 
-string *brakeInto(string buffer, int *size, char c)
-{
-    string *array;
-    int i = 0, j = 0;
+// string *brakeInto(string buffer, int *size, char c)
+// {
+//     string *array;
+//     int i = 0, j = 0;
 
-    for (i = 0; i < strlen(buffer); i++)
-    {
-        if (buffer[i] == c)
-        {
-            j++;
-        }
-    }
+//     for (i = 0; i < strlen(buffer.len); i++)
+//     {
+//         if (buffer.value[i] == c)
+//         {
+//             j++;
+//         }
+//     }
 
-    array = (string *)malloc(sizeof(string) * j);
-    *size = j;
+//     array = (string *)malloc(sizeof(string) * j);
+//     *size = j;
 
-    j = 0;
-    for (i = 0; i < strlen(buffer); i++)
-    {
-        if (buffer[i] == c)
-        {
-            array[j] = (string)malloc(sizeof(char) * i);
-            strncpy(array[j], buffer, i);
-            j++;
+//     j = 0;
+//     for (i = 0; i < strlen(buffer); i++)
+//     {
+//         if (buffer[i] == c)
+//         {
+//             array[j] = (string)malloc(sizeof(char) * i);
+//             strncpy(array[j], buffer, i);
+//             j++;
 
-            buffer += i + 1;
-            i = -1;
-        }
-    }
+//             buffer += i + 1;
+//             i = -1;
+//         }
+//     }
 
-    return array;
-}
+// return array;
+// }
 
 /*
 Dado bytes, retorna um array de 4 bytes, em little Endian
@@ -82,31 +93,31 @@ Dado bytes, retorna um array de 4 bytes, em little Endian
 Se fillFF for 1, o resto do array é preenchido com 0xFF.
 Caso o contrario com 0x00.
 */
-byte *littleThatEndian(byte *bytes, int fillFF)
-{
-    byte *array = malloc(sizeof(byte) * 4);
-    array[0] = bytes[3];
-    array[1] = bytes[2];
-    array[2] = bytes[1];
-    array[3] = bytes[0];
-    if (fillFF)
-    {
-        array[0] = 0xFF;
-        array[1] = 0xFF;
-        array[2] = 0xFF;
-        array[3] = 0xFF;
-    }
+// byte *littleThatEndian(byte *bytes, int fillFF)
+// {
+//     byte *array = malloc(sizeof(byte) * 4);
+//     array[0] = bytes[3];
+//     array[1] = bytes[2];
+//     array[2] = bytes[1];
+//     array[3] = bytes[0];
+//     if (fillFF)
+//     {
+//         array[0] = 0xFF;
+//         array[1] = 0xFF;
+//         array[2] = 0xFF;
+//         array[3] = 0xFF;
+//     }
 
-    else
-    {
-        array[0] = 0x00;
-        array[1] = 0x00;
-        array[2] = 0x00;
-        array[3] = 0x00;
-    }
+//     else
+//     {
+//         array[0] = 0x00;
+//         array[1] = 0x00;
+//         array[2] = 0x00;
+//         array[3] = 0x00;
+//     }
 
-    return array;
-}
+//     return array;
+// }
 
 /*
 Dados um inteiro, retorna um array de 4 bytes, em little Endian
@@ -123,18 +134,11 @@ byte *intToBytes(int x, int fillFF)
 
     if (fillFF)
     {
-        array[0] = 0xFF;
-        array[1] = 0xFF;
-        array[2] = 0xFF;
-        array[3] = 0xFF;
-    }
-
-    else
-    {
-        array[0] = 0x00;
-        array[1] = 0x00;
-        array[2] = 0x00;
-        array[3] = 0x00;
+        for (int i = 0; i < 4; i++)
+        {
+            if (array[i] == 0x00)
+                array[i] = 0xFF;
+        }
     }
 
     return array;
